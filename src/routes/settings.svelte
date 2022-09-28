16d5d480525598bac804602740f40a2ec8b1436e
atampy25 <24306974+atampy25@users.noreply.github.com>
2022-09-28 22:14:25 +1000

    Information page and feedback survey

--- src/routes/settings.svelte
+++ src/routes/settings.svelte
@@ -13,45 +13,78 @@
 	})()
 </script>
 
-<h1>Settings</h1>
-<div class="flex items-center gap-2">
-	<div class="flex-shrink"><Checkbox bind:checked={$appSettings.gameFileExtensions} labelText="Enable game-file extensions" /></div>
-	<TooltipIcon icon={Information}>
-		<span slot="tooltipText" style="font-size: 0.875rem; margin-top: 0.5rem; margin-bottom: 0.5rem">
-			QuickEntity Editor has a set of extensions that make it far easier to work with game entities, including the ability to quickly load any game entity, the ability to preview overrides, 3D
-			previews for sub-entities and deep intellisense. These require extracting up to 8 gigabytes of game files, however.
-		</span>
-	</TooltipIcon>
-</div>
-<br />
-{#if $appSettings.gameFileExtensions}
-	<TextInput labelText="Path to game file data" placeholder={documentsPath + "blabla"} bind:value={$appSettings.gameFileExtensionsDataPath} />
-	<br />
-{/if}
-<div class="flex items-center gap-2">
-	<div class="flex-shrink">
-		<Checkbox
-			bind:checked={$appSettings.inVivoExtensions}
-			on:change={async () => {
-				if ($appSettings.inVivoExtensions) {
-					await copyFile("LogPins.dll", await join($appSettings.retailPath, "mods", "LogPins.dll"))
-					await copyFile("LogPins.pdb", await join($appSettings.retailPath, "mods", "LogPins.pdb"))
-				} else {
-					await removeFile(await join($appSettings.retailPath, "mods", "LogPins.dll"))
-					await removeFile(await join($appSettings.retailPath, "mods", "LogPins.pdb"))
-				}
-			}}
-			labelText="Enable in-vivo extensions"
-		/>
+<div class="flex flex-col h-full">
+	<div>
+		<h1>Settings</h1>
+		<div class="flex items-center gap-2">
+			<div class="flex-shrink"><Checkbox bind:checked={$appSettings.gameFileExtensions} labelText="Enable game-file extensions" /></div>
+			<TooltipIcon icon={Information}>
+				<span slot="tooltipText" style="font-size: 0.875rem; margin-top: 0.5rem; margin-bottom: 0.5rem">
+					QuickEntity Editor has a set of extensions that make it far easier to work with game entities, including the ability to quickly load any game entity, the ability to preview
+					overrides, 3D previews for sub-entities and deep intellisense. These require extracting up to 8 gigabytes of game files, however.
+				</span>
+			</TooltipIcon>
+		</div>
+		<br />
+		{#if $appSettings.gameFileExtensions}
+			<TextInput labelText="Path to game file data" placeholder={documentsPath + "blabla"} bind:value={$appSettings.gameFileExtensionsDataPath} />
+			<br />
+		{/if}
+		<div class="flex items-center gap-2">
+			<div class="flex-shrink">
+				<Checkbox
+					bind:checked={$appSettings.inVivoExtensions}
+					on:change={async () => {
+						if ($appSettings.inVivoExtensions) {
+							await copyFile("LogPins.dll", await join($appSettings.retailPath, "mods", "LogPins.dll"))
+							await copyFile("LogPins.pdb", await join($appSettings.retailPath, "mods", "LogPins.pdb"))
+						} else {
+							await removeFile(await join($appSettings.retailPath, "mods", "LogPins.dll"))
+							await removeFile(await join($appSettings.retailPath, "mods", "LogPins.pdb"))
+						}
+					}}
+					labelText="Enable in-vivo extensions"
+				/>
+			</div>
+			<TooltipIcon icon={Information}>
+				<span slot="tooltipText" style="font-size: 0.875rem; margin-top: 0.5rem; margin-bottom: 0.5rem">
+					If you're using a version of the game with a working ZHMModSDK installed, you can enable a set of extensions that allow you to work with entities while the game is running,
+					including highlighting them in-game and setting properties without requiring a game restart.
+				</span>
+			</TooltipIcon>
+		</div>
+		<br />
+		<TextInput labelText="Runtime path" placeholder={documentsPath + "blabla"} bind:value={$appSettings.runtimePath} />
+		<br />
+		<TextInput labelText="Retail path" placeholder={documentsPath + "blabla"} bind:value={$appSettings.retailPath} />
+		<br />
+		<h1>Information</h1>
+		<p>
+			QuickEntity Editor is licensed under the GNU General Public License version 3.0 (GPLv3). The in-vivo extensions are a result of work from primarly piepieonline, re-compiled for the newest
+			SDK version by invalid. The ZHMModSDK it uses was created by NoFaTe (OrfeasZ), to whom we owe, in part, the current state of Hitman modding to. QNE also includes RPKGv3, which is licensed
+			under MIT and developed by a number of contributors, including primarily 2kpr.
+		</p>
+		<br />
+		<h1>Feedback survey</h1>
+		<br />
 	</div>
-	<TooltipIcon icon={Information}>
-		<span slot="tooltipText" style="font-size: 0.875rem; margin-top: 0.5rem; margin-bottom: 0.5rem">
-			If you're using a version of the game with a working ZHMModSDK installed, you can enable a set of extensions that allow you to work with entities while the game is running, including
-			highlighting them in-game and setting properties without requiring a game restart.
-		</span>
-	</TooltipIcon>
+	<div class="overflow-y-auto flex-grow">
+		<iframe data-tally-src="https://tally.so/embed/3xXq0r?hideTitle=1" width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="QuickEntity Editor feedback" />
+		<script>
+			var d = document,
+				w = "https://tally.so/widgets/embed.js",
+				v = function () {
+					"undefined" != typeof Tally
+						? Tally.loadEmbeds()
+						: d.querySelectorAll("iframe[data-tally-src]:not([src])").forEach(function (e) {
+								e.src = e.dataset.tallySrc
+						  })
+				}
+			if (d.querySelector('script[src="' + w + '"]')) v()
+			else {
+				var s = d.createElement("script")
+				;(s.src = w), (s.onload = v), (s.onerror = v), d.body.appendChild(s)
+			}
+		</script>
+	</div>
 </div>
-<br />
-<TextInput labelText="Runtime path" placeholder={documentsPath + "blabla"} bind:value={$appSettings.runtimePath} />
-<br />
-<TextInput labelText="Retail path" placeholder={documentsPath + "blabla"} bind:value={$appSettings.retailPath} />
