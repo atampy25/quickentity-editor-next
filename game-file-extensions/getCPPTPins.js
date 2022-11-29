40cc59179ef61dd97644263e0103b30264db7857
atampy25 <24306974+atampy25@users.noreply.github.com>
2022-11-29 22:29:02 +1000

    Enhanced output pin crawling

--- game-file-extensions/getCPPTPins.js
+++ game-file-extensions/getCPPTPins.js
@@ -27,6 +27,7 @@ const allCPPTs = new Set(
 )
 
 let data = {}
+let i = 0
 let x = 0
 
 let hashes = Object.fromEntries(
@@ -36,21 +37,101 @@ let hashes = Object.fromEntries(
 		.map((a) => [a.trim().split(",").slice(1).join(","), a.trim().split(".")[0]])
 )
 
+const cpptEntityRootsSet = new Set()
+const cpptEntityRootsData = {}
+
 for (let filePath of allFiles) {
 	filePath = filePath.path.replace(".TEMP.json", ".TEMP.entity.json")
 
 	if (fs.existsSync(filePath)) {
+		/** @type import("../src/lib/quickentity-types").Entity */
 		const entity = readLossless(filePath)
-		for (const subEntity of Object.values(entity.entities)) {
-			if (allCPPTs.has(subEntity.template)) {
+		if (allCPPTs.has(entity.entities[entity.rootEntity].template)) {
+			const discount = new Set()
+
+			for (const subEntity of Object.values(entity.entities)) {
+				// the root entity is a class entity, therefore anything that isn't forwarded must be emitted by that class;
+				// therefore any uses of events in other entities that aren't forwards from here must be emitted by that class
+
+				if (subEntity.outputCopying) {
+					for (const data of Object.values(subEntity.outputCopying)) {
+						for (const [evtToSend, ents] of Object.entries(data)) {
+							for (const ent of ents) {
+								if (ent === entity.rootEntity) {
+									discount.add(evtToSend)
+								}
+							}
+						}
+					}
+				}
+			}
+
+			cpptEntityRootsSet.add(entity.tempHash)
+			cpptEntityRootsData[entity.tempHash] = {
+				cppt: entity.entities[entity.rootEntity].template,
+				discount
+			}
+		}
+	}
+
+	i++
+
+	if (i % 1000 == 0) {
+		console.log(`Stage 1: Done ${i} of ${allFiles.length}`)
+	}
+}
+
+for (let filePath of allFiles) {
+	filePath = filePath.path.replace(".TEMP.json", ".TEMP.entity.json")
+
+	if (fs.existsSync(filePath)) {
+		/** @type import("../src/lib/quickentity-types").Entity */
+		const entity = readLossless(filePath)
+		for (const [subEntityID, subEntity] of Object.entries(entity.entities)) {
+			const isCPPTRooted = cpptEntityRootsSet.has(subEntity.template)
+			if (isCPPTRooted || allCPPTs.has(subEntity.template)) {
 				if (subEntity.events) {
-					data[subEntity.template] ??= { output: new Set() }
-					data[subEntity.template].output.add(...Object.keys(subEntity.events))
+					data[isCPPTRooted ? cpptEntityRootsData[subEntity.template].cppt : subEntity.template] ??= { output: new Set() }
+
+					const outputsToAdd = Object.keys(subEntity.events)
+						.filter(
+							// check that this event is not a forwarding (as if it isn't a forwarding it must be sent by the entity itself)
+							(evt) =>
+								!Object.values(entity.entities).some(
+									(a) =>
+										a.outputCopying &&
+										Object.entries(a.outputCopying).some(([evtToCopy, data]) =>
+											Object.entries(data).some(([evtToSend, ents]) => evtToSend === evt && ents.some((ent) => ent === subEntityID))
+										)
+								)
+						)
+						.filter(isCPPTRooted ? (evt) => !cpptEntityRootsData[subEntity.template].discount.has(evt) : (evt) => true)
+
+					if (outputsToAdd.length) {
+						data[isCPPTRooted ? cpptEntityRootsData[subEntity.template].cppt : subEntity.template].output.add(...outputsToAdd)
+					}
 				}
 
 				if (subEntity.outputCopying) {
-					data[subEntity.template] ??= { output: new Set() }
-					data[subEntity.template].output.add(...Object.keys(subEntity.outputCopying))
+					data[isCPPTRooted ? cpptEntityRootsData[subEntity.template].cppt : subEntity.template] ??= { output: new Set() }
+
+					const outputsToAdd = Object.keys(subEntity.outputCopying)
+						.filter(
+							// check that this event is not a forwarding (as if it isn't a forwarding it must be sent by the entity itself)
+							(evt) =>
+								!Object.values(entity.entities).some(
+									(a) =>
+										a.outputCopying &&
+										Object.entries(a.outputCopying).some(([evtToCopy, data]) =>
+											Object.entries(data).some(([evtToSend, ents]) => evtToSend === evt && ents.some((ent) => ent === subEntityID))
+										)
+								)
+						)
+						.filter(isCPPTRooted ? (evt) => !cpptEntityRootsData[subEntity.template].discount.has(evt) : (evt) => true)
+
+					if (outputsToAdd.length) {
+						data[isCPPTRooted ? cpptEntityRootsData[subEntity.template].cppt : subEntity.template].output.add(...outputsToAdd)
+					}
 				}
 			}
 		}
@@ -59,7 +140,7 @@ for (let filePath of allFiles) {
 	x++
 
 	if (x % 1000 == 0) {
-		console.log(`Done ${x} of ${allFiles.length}`)
+		console.log(`Stage 2: Done ${x} of ${allFiles.length}`)
 
 		fs.writeJSONSync("./pins.json", Object.fromEntries(Object.entries(data).map((a) => [a[0], { output: [...a[1].output] }])))
 	}
