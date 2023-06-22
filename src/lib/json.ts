224fe10e0ba778bab425a7031b932d09c6fcd748
atampy25 <24306974+atampy25@users.noreply.github.com>
2023-06-22 18:18:21 +1000

    Documentation for all property types

--- src/lib/json.ts
+++ src/lib/json.ts
@@ -19,7 +19,7 @@ function replacer(key: string, value: any) {
 }
 
 // eslint-disable-next-line no-control-regex
-export const parse = (val: string) => JSON.parse(val.replace(/(\\(?:["\\bfnrt]|u[0-9a-fA-F])|[^\u0000-\u001F\\])":([-+Ee0-9.]+)/g, "$1\": \"~|$2\""), reviver)
+export const parse = (val: string) => JSON.parse(val.replace(/(\\(?:["\\bfnrt]|u[0-9a-fA-F])|[^\u0000-\u001F\\])":(?: *)([-+Ee0-9.]+)/g, "$1\":\"~|$2\""), reviver)
 export const stringify = (val: Record<string, any>, sp: string | undefined = undefined) => LosslessJSON.stringify(val, replacer, sp)
 
 export default {
